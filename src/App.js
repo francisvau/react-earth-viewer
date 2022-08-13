6454cb38f15a5db49649c8f21e22a7534c8504de
francisvau <vauterinf@gmail.com>
2022-08-13 17:26:47 +0200

    dates fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import Header from "./components/Header";
+import ImageViewer from "./components/ImageViewer";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <ImageViewer />
     </div>
   );
 }
--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -1,7 +1,25 @@
+import { useState, useEffect } from "react"
 
 const ImageViewer = () => {
+
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+        setLoading(true)
+        const res = await fetch('https://epic.gsfc.nasa.gov/api/natural/all')
+        const dates = await res.json()
+        setData(dates.map(e => e["date"]))
+        setLoading(false)
+    }
+    fetchEvents()
+  }, [])
+
   return (
-    <div>ImageViewer</div>
+    <div>
+        {data.length === 0 ? "another" : "no one"}
+    </div>
   )
 }
 
